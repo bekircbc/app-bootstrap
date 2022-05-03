2d7a10d2a7184c9d13286c138896ca99eeca4923
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-03 09:49:15 +0200

    styled with bootstrap..

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Company Site</h1>
         <button className="btn-primary me-1">Employees</button>
-        <button className="btn-primary">Customers</button>
+        <button className="btn-success">Customers</button>
       </div>
     </div>
   );
