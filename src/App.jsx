62bd4b89ae6fdecfd7e83420e6839b6cea59e9aa
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-03 10:11:12 +0200

    Card component from bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/bootstrap.darkly.min.css";
-import "./styles/bootstrap.morph.min.css";
-import "./styles/bootstrap.quartz.min.css";
-import { Container, Button } from "react-bootstrap";
+// import "./styles/bootstrap.morph.min.css";
+// import "./styles/bootstrap.quartz.min.css";
+import { Container, Button, Card } from "react-bootstrap";
 
 function App() {
   return (
@@ -11,6 +11,9 @@ function App() {
         <h1>Company Site</h1>
         <Button className="me-1">Employees</Button>
         <Button className="btn-success">Customers</Button>
+        <Card className="mt-2">
+          <Card.Body>This is the some text within a card body.</Card.Body>
+        </Card>
       </Container>
     </div>
   );
