0f4439a26a400a682c1d424962d6e29b563b83f4
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-03 11:01:22 +0200

    customer page with table added

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,7 +1,33 @@
+import customers from "../data/customers.json";
+import { Table } from "react-bootstrap";
+
 export const Customers = () => {
   return (
     <>
       <h2>Customers</h2>
+      <p>This is the customers page.</p>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.slice(1, 10).map((cust, index) => {
+            return (
+              <tr>
+                <td>{cust.customerID}</td>
+                <td>{cust.contactName.split(" ")[0]}</td>
+                <td>{cust.contactName.split(" ")[1]}</td>
+                <td>{cust.companyName}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </>
   );
 };
