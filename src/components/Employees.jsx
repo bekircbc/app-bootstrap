3f7024ff720c85095f5388177aaae9f77c9cb009
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-03 11:00:00 +0200

    employee page changed and carousel from bootstrap added

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,7 +1,28 @@
+import employees from "../data/employees.json";
+import { Carousel } from "react-bootstrap";
+
 export const Employees = () => {
   return (
     <>
       <h2>Employees</h2>
+      <p>This is the employees page.</p>
+      <Carousel style={{ width: "300px" }}>
+        {employees.map((emp, index) => {
+          return (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={`images/employees/employee_${emp.employeeID}.jpg`}
+              />
+              <Carousel.Caption>
+                <div>
+                  {emp.firstName} {emp.lastName}
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
     </>
   );
 };
